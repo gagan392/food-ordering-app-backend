901685e349e8be980e215660c56a1cfe83f490ad
Gagan Raj M <gagan.rajm@ellucian.com>
2018-12-07 01:58:41 +0530

    Endpoint for changing password.

--- src/main/java/org/upgrad/services/UserServiceImpl.java
+++ src/main/java/org/upgrad/services/UserServiceImpl.java
@@ -2,16 +2,13 @@ package org.upgrad.services;
 
 import org.springframework.stereotype.Service;
 import org.upgrad.models.User;
-import org.upgrad.models.UserAuthToken;
-import org.upgrad.repositories.UserAuthTokenRepository;
 import org.upgrad.repositories.UserRepository;
 
 import javax.transaction.Transactional;
-import java.util.Optional;
 
 @Service
 @Transactional
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
@@ -34,4 +31,10 @@ public class UserServiceImpl implements UserService{
         User user = new User(firstName, lastName, email, contactNumber, password);
         userRepository.save(user);
     }
+
+    @Override
+    public void updateUser(User user) {
+        userRepository.save(user);
+    }
+
 }
