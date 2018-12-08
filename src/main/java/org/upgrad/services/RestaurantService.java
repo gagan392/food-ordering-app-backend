040aa8c47adeafec9270f1c5dbd7914abb94d22c
Gagan Raj M <gagan.rajm@ellucian.com>
2018-12-09 02:15:39 +0530

    Added Get “/restaurant” (all restaurants) endpoint.

--- src/main/java/org/upgrad/services/RestaurantService.java
+++ src/main/java/org/upgrad/services/RestaurantService.java
@@ -1,4 +1,11 @@
 package org.upgrad.services;
 
+import org.upgrad.models.Restaurant;
+
+import java.util.List;
+
 public interface RestaurantService {
+
+    List<Restaurant> getAll();
+
 }
--- src/main/java/org/upgrad/services/RestaurantServiceImpl.java
+++ src/main/java/org/upgrad/services/RestaurantServiceImpl.java
@@ -1,4 +1,24 @@
 package org.upgrad.services;
 
-public class RestaurantServiceImpl{
+import com.google.common.collect.Lists;
+import org.springframework.stereotype.Service;
+import org.upgrad.models.Restaurant;
+import org.upgrad.repositories.RestaurantRepository;
+
+import java.util.List;
+
+@Service
+public class RestaurantServiceImpl implements RestaurantService {
+
+    private final RestaurantRepository restaurantRepository;
+
+    public RestaurantServiceImpl(RestaurantRepository restaurantRepository) {
+        this.restaurantRepository = restaurantRepository;
+    }
+
+    @Override
+    public List<Restaurant> getAll() {
+        List<Restaurant> restaurants = restaurantRepository.getAll();
+        return restaurants;
+    }
 }
